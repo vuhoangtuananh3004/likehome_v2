f60f9abbb1c814a3f644e7d7b1d9910e10f150e7
Anh Vu <anh@Anhs-MacBook-Pro.local>
2022-11-20 21:43:36 -0800

    update

--- next.config.js
+++ next.config.js
@@ -5,7 +5,8 @@ const nextConfig = {
   images:{
     domains:['images.unsplash.com','images.pexels.com','a0.muscache.com','www.shutterstock.com'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-  }
+  },
+  localePath: path.resolve('./public/locales'),
 }
 
 module.exports = nextConfig
