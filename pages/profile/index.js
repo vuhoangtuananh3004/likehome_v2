94cff8ac39923cb7621f04bea4da2bafb9d9146f
Tuan Anh <vuhoangtuananh30041994@gmail.com>
2022-11-27 05:43:40 -0800

    test

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,10 +1,10 @@
-import React from 'react'
-import MainProfile from '../../components/Profile/MainProfile'
-import Profile from '../../components/Profile/Profile'
-function index() {
-  return (
-    <MainProfile/>
-  )
+import React from "react";
+import { useSelector } from "react-redux";
+import MainProfile from "../../components/Profile/MainProfile";
+
+function Index() {
+
+  return <MainProfile /> 
 }
 
-export default index
+export default Index;
